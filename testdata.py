e67d673d83c10cdb3b8839f55db58350e45310a6
Jascha Petter <jascha.petter@student.uni-tuebingen.de>
2016-06-15 09:28:38 +0200

    add random Data skript

---
+++ testdata.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import dbc
+import time
+import random
+
+dbc.db.query("""SELECT COUNT(*) FROM `Data`;""")
+count = (dbc.db.store_result().fetch_row()[0])[0]
+
+t = time.time()
+
+for i in range(0, 5000-count):
+    #Sys. time in UNIX epoch
+    ts = str(t+i*.01)
+    #random ACC values
+    ACC_X = str(random.randint(-25500,25500)*0.01)
+    ACC_Y = str(random.randint(-25500,25500)*0.01)
+    ACC_Z = str(random.randint(-25500,25500)*0.01)
+    #random MAG values
+    MAG_X = str(random.randint(-25500,25500)*0.01)
+    MAG_Y = str(random.randint(-25500,25500)*0.01)
+    MAG_Z = str(random.randint(-25500,25500)*0.01)
+    #random GYRO values
+    G_YAW = str(random.randint(-25500,25500)*0.01)
+    G_PITCH = str(random.randint(-25500,25500)*0.01)
+    G_ROLL= str(random.randint(-25500,25500)*0.01)
+    #random TEMP and PRESS values
+    TEMP = str(random.randint(-25500,25500)*0.01)
+    PRESS = str(random.randint(-25500,25500)*0.01)
+    #inserting data into DB
+    dbc.db.query("""
+    INSERT INTO `SenseData`.`DATA`
+    (`PITIME`,
+    `ACC_X`,`ACC_Y`,`ACC_Z`,
+    `MAG_X`,`MAG_Y`,`MAG_Z`,
+    `G_YAW`,`G_PITCH`,`G_ROLL`,
+    `TEMP`,`PRESS`)
+    VALUES
+    (FROM_UNIXTIME("""+ts+"""),
+    """+ACC_X+""","""+ACC_Y+""","""+ACC_Z+""",
+    """+MAG_X+""","""+MAG_Y+""","""+MAG_Z+""",
+    """+G_YAW+""","""+G_PITCH+""","""+G_ROLL+""",
+    """+TEMP+""","""+PRESS+""");
+    """)
+
+dbc.db.commit()
+dbc.db.close()
