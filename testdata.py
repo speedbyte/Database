f476b081f59b566f9625baff9092e7989d521ddf
Jascha Petter <jascha.petter@student.uni-tuebingen.de>
2016-06-22 15:13:14 +0200

    add random motor values

--- testdata.py
+++ testdata.py
@@ -26,20 +26,27 @@ for i in range(0, 5000-count):
     #random TEMP and PRESS values
     TEMP = str(random.randint(-25500,25500)*0.01)
     PRESS = str(random.randint(-25500,25500)*0.01)
+    #ramdom MOTOR values
+    M1 = str(random.randint(-25500,25500)*0.01)
+    M2 = str(random.randint(-25500,25500)*0.01)
+    M3 = str(random.randint(-25500,25500)*0.01)
+    M4 = str(random.randint(-25500,25500)*0.01)
     #inserting data into DB
     dbc.db.query("""
     INSERT INTO `SenseData`.`DATA`
     (`PITIME`,
     `ACC_X`,`ACC_Y`,`ACC_Z`,
     `MAG_X`,`MAG_Y`,`MAG_Z`,
-    `G_YAW`,`G_PITCH`,`G_ROLL`,
-    `TEMP`,`PRESS`)
+    `G_ROLL`,`G_PITCH`,`G_YAW`,
+    `TEMP`,`PRESS`,
+    `M1`,`M2`,`M3`,`M4`)
     VALUES
     (FROM_UNIXTIME("""+ts+"""),
     """+ACC_X+""","""+ACC_Y+""","""+ACC_Z+""",
     """+MAG_X+""","""+MAG_Y+""","""+MAG_Z+""",
-    """+G_YAW+""","""+G_PITCH+""","""+G_ROLL+""",
-    """+TEMP+""","""+PRESS+""");
+    """+G_ROLL+""","""+G_PITCH+""","""+G_YAW+""",
+    """+TEMP+""","""+PRESS+""",
+    """+M1+""","""+M2+""","""+M3+""","""+M4+""");
     """)
 
 dbc.db.commit()
